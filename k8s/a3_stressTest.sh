d824fd661fe4b7e073776ee1f4086176b7fd750f
blaketingyu <blaketantingyu@gmail.com>
2022-09-08 00:28:45 +0800

    automate stress test

--- k8s/a3_stressTest.sh
+++ k8s/a3_stressTest.sh
@@ -1,7 +1,9 @@
 echo A3 Stress Test
 echo ==========================
 
-kubectl describe hpa --context kind-kind-1
+kubectl describe hpa --context kind-kind-l
+
+while true; clear; do kubectl describe hpa;sleep 5; done
 
 curl "localhost/?[0-4999]" --parallel --parallel-max 100 --no-progress-meter > /dev/null
 
